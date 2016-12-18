1b1e0616e40884a9adbe3ef7c73c8b9c899fb3ef
logalex96 <logalex96@gmail.com>
2016-12-18 21:33:28 +0200

    Hello MineSweeper !
    My bby took his first breath... there is more work to be done

---
+++ header.h
@@ -0,0 +1,31 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <ctime>
+#include <iostream>
+#define MAX 1000
+
+/*
+0 is empty
+1-8 is number
+9 is mine
+10 is hide square
+11 is flag
+*/
+
+// Definitions
+int width, height, squareNumber, mineNumber, unrevealed, flagNum;
+int squareSize = 32;
+int board[MAX*MAX] = {0};
+int boardRevealed[MAX*MAX] = {0}; // 0 is hide, 1 is show, 2 is flag
+bool gameOver = false;
+
+// My Colors
+sf::Color Background_color(192, 192, 192);
+sf::Color MyBlack(0, 0, 0, 255);
+
+// Functions
+int nearMines(int x, int y);
+void countSurrounding(int x, int y);
+void placeMines();
+bool revealed (int x, int y);
+void reveal(int x, int y);
---
+++ main.cpp
@@ -0,0 +1,132 @@
+#include "header.h"
+
+int main()
+{
+    srand(time(0));
+    width=15;
+    height=15;
+    mineNumber=40;
+    squareNumber= width * height;
+    unrevealed= squareNumber;
+
+    sf::RenderWindow appWindow(sf::VideoMode(width*squareSize, height*squareSize), "Minesweeper!");
+    sf::Texture t;
+    t.loadFromFile("images/tiles.png");
+    sf::Sprite s(t);
+
+    // first things first (create the board)
+    for(int i=0; i <squareNumber; i++)
+    {
+        board[i]=0;
+        boardRevealed[i]=0;
+    }
+
+    placeMines();
+
+    for(int i=0; i<squareNumber; i++)
+        countSurrounding(i%width, i/width);
+
+    while(appWindow.isOpen())
+    {
+        sf::Vector2i pos = sf::Mouse::getPosition(appWindow);
+        int mouseX = pos.x/squareSize;
+        int mouseY = pos.y/squareSize;
+
+        sf::Event e;
+        while(appWindow.pollEvent(e))
+        {
+            if(e.type == sf::Event::Closed)
+                appWindow.close();
+
+            if(e.type == sf::Event::MouseButtonPressed && gameOver !=1 )
+            {
+                if(e.key.code == sf::Mouse::Left)
+                {
+                    if(boardRevealed[mouseY * width + mouseX] == 0) reveal(mouseX, mouseY);
+                }
+                else if (e.key.code == sf::Mouse::Right)
+                {
+                    if (boardRevealed[mouseY * width + mouseX] == 2)
+                        boardRevealed[mouseY * width + mouseX] = 0;
+                    else if (boardRevealed[mouseY * width + mouseX] == 0)
+                        boardRevealed[mouseY * width + mouseX] = 2;
+                }
+            }
+        }
+
+        appWindow.clear(Background_color);
+
+        for (int i=0; i<squareNumber; i++)
+        {
+            if (boardRevealed[i] == 1)
+                s.setTextureRect(sf::IntRect(board[i]*squareSize, 0, squareSize, squareSize));
+            else if (boardRevealed[i] == 0)
+                s.setTextureRect(sf::IntRect(10*squareSize, 0, squareSize, squareSize));
+            else if (boardRevealed[i] == 2)
+                s.setTextureRect(sf::IntRect(11*squareSize, 0, squareSize, squareSize));
+            s.setPosition(i%width*squareSize, i/width*squareSize);
+            appWindow.draw(s);
+        }
+        appWindow.display();
+    }
+    return 0;
+}
+
+int nearMines(int x, int y)
+{
+    if (x>=0 && x<width && y>=0 && y<=height)
+    {
+        if (board[y*width+x] == 9) return 1;
+        else return 0;
+    }
+    else return 0;
+}
+
+void countSurrounding(int x, int y)
+{
+    if(board[y*width+x] == 9) return;
+    int result = 0;
+    if(nearMines(x+1, y)) result++;
+    if(nearMines(x-1, y)) result++;
+    if(nearMines(x, y-1)) result++;
+    if(nearMines(x, y+1)) result++;
+    if(nearMines(x+1, y+1)) result++;
+    if(nearMines(x+1, y-1)) result++;
+    if(nearMines(x-1, y+1)) result++;
+    if(nearMines(x-1, y-1)) result++;
+    board[y*width+x]= result;
+}
+
+void placeMines()
+{
+    int count= 0;
+    while(count < mineNumber)
+    {
+        int randPosition= rand()%squareNumber;
+        if(board[randPosition] != 9)
+        {
+            board[randPosition]= 9;
+            ++count;
+        }
+    }
+}
+
+bool revealed (int x, int y)
+{
+    if(x<0 || x>=width || y<0 || y>=height) return true;
+    if(boardRevealed[y*width+x] != 0) return true;
+    return false;
+}
+
+void reveal(int x, int y)
+{
+    if(x<0 || x>=width || y<0 || y>=height) return;
+    int i= y*width+x;
+    if(boardRevealed[i] == 0) {
+        boardRevealed[i] = 1;
+        --unrevealed;
+        if(board[i] == 9)
+            gameOver = true;
+    }
+}
+
