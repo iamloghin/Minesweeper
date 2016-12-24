3586dd0b71b6e07a8da68da1e6d239c76d519070
logalex96 <logalex96@gmail.com>
2016-12-24 17:14:48 +0200

    Sounds + THE END
    I added sounds in the whole game + the final text for cases "you won"
    and "you lose" + dependent sfml files
    ___ I use codebeautify to embellish a bit the code :)

--- header.h
+++ header.h
@@ -15,9 +15,9 @@
 // Definitions
 int width, height, squareNumber, mineNumber, unrevealed, flagNum;
 int squareSize = 32;
-int board[MAX*MAX] = {0};
-int boardRevealed[MAX*MAX] = {0}; // 0 is hide, 1 is show, 2 is flag
-bool gameOver = false;
+int board[MAX * MAX] = { 0 };
+int boardRevealed[MAX * MAX] = { 0 }; // 0 is hide, 1 is show, 2 is flag
+bool gameOver = false, won = false;
 
 // My Colors
 sf::Color Background_color(192, 192, 192);
@@ -27,6 +27,6 @@ sf::Color MyBlack(0, 0, 0, 255);
 int nearMines(int x, int y);
 void countSurrounding(int x, int y);
 void placeMines();
-bool revealed (int x, int y);
+bool revealed(int x, int y);
 void display(int x, int y, int firstX, int firstY);
 void reveal(int x, int y);
